51b3f79b4914cffc9a688694c0134e7035eee72e
Douglasddsa <douglasddsa@gmail.com>
2017-11-17 16:08:20 -0200

    Adicionando o código para o Sensor Microfone

---
+++ microfone_db.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import wave
+import numpy as np
+import matplotlib
+matplotlib.use('Pdf')
+#import matplotlib.pyplot as plt
+import math
+
+wf = wave.open('/home/pi/Desktop/Douglas/teste.wav', 'r')
+fs = wf.getframerate()
+ts=fs
+
+sinal =(wf.readframes(fs))
+ref=32767
+
+Amplitude = (np.fromstring(sinal, dtype=np.int16))/(np.sqrt(2))
+
+rms = np.sqrt(np.mean(Amplitude**2))
+dbspl = 94 + (20*np.log10(rms/ref))
+
+# Janela de Hamming
+win = np.hamming(len(Amplitude))
+Amplitude = (Amplitude * win)
+
+AMP = np.fft.fft(Amplitude)
+FREQ = np.fft.fftfreq(ts, 1.0/(len(Amplitude)))
+
+#Corrigindo a magnitude da FFT pela janela de Hamming e a energia do sinal
+#multiplicando por 2 por conta da utilização de metade da FFT
+
+AMP_MAG = np.abs(AMP) * 2 / np.sum(win)
+
+# Para obter o valor RMS da FFT, basta multiplica por raiz de 2
+AMP_RMS = AMP_MAG / np.sqrt(2)
+
+# Deslocando as amostras de magnitude e frequência para obter  
+# o gráfico em db
+FREQ   = np.fft.fftshift(FREQ)
+AMP_RMS = np.fft.fftshift(AMP_RMS)
+
+# Conversão para escala em DB
+AMP_DB = (20 * np.log10( AMP_RMS/ref )) + 94
+
+###plt.semilogx(FREQ, AMP_DB)
+###plt.figure(1)
+###plt.xlim( (0, fs/2)  )
+###plt.ylim( (-200, 200))
+
+#plt.figure(2)
+#plt.plot(FREQ, AMP_RMS)
+#plt.grid('on')
+
+###plt.show()
+# Comparando as Amplitudes
+print dbspl, AMP_DB.max()
+
+#Valor definido como inicio do choro do bebê db=50
+#Escrita em arquivo
+
+if (dbspl >=50 or (AMP_DB.max() >= 60) ):
+	arquivo= open('/home/pi/Desktop/Douglas/arq_estado.txt', 'w')
+	arquivo.write("{\"estado\": \"criança chorando\"}")
+	arquivo.close()
+
+else:
+	arquivo= open('/home/pi/Desktop/Douglas/arq_estado.txt', 'w')
+	arquivo.write("{\"estado\": \"não está chorando\"}")
+	arquivo.close()
+
+
+
+
---
+++ processos_pi2.c
@@ -0,0 +1,55 @@
+#include<stdio.h>		//lib para usar fprintf() e perror()
+#include<stdlib.h>		//lib para usar macros EXIT_SUCCESS e EXIT_FAILURE
+#include<unistd.h>		//lib para usar fork(), getpid() e getppid()
+#include<sys/types.h>		//lib para usar tipo pid_t
+#include<sys/wait.h>		//lib para usar wait()
+
+
+
+int main(){
+  pid_t child_pid;
+  int status;
+
+	/* Cria processo filho */
+	child_pid = fork();
+
+	if( child_pid == 0 ){
+		/* Processo filho. */
+
+		fprintf(stdout, " Iniciando Processo Filho. Possui PID: %d.\n", (int)getpid() );
+		fprintf(stdout, "PID do Processo Pai: %d.\n", (int)getppid() );
+		fprintf(stdout, "Iniciando processo para gravação de audio pelo microfone");
+		system("cd /home/pi/Desktop/Douglas");
+		system("rec -r 48000 -c1 teste.wav trim 0 5");
+		//sleep(4);
+		_exit(EXIT_SUCCESS);
+
+	}
+	else if( child_pid < 0 ){
+		/* Se houver erro ao criar processo filho */
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else{
+		/* Código do processo pai continua aqui */
+		/* Aguarda processo filho terminar e recebe status */
+		wait(& status);
+
+		/* Verifica se o processo filho terminou normalmente */
+		/* Caso tenha terminado, imprime o status de saída do filho */
+		if( WIFEXITED(status) )
+			fprintf(stdout, "Status de saida do filho: %d.\n", WEXITSTATUS(status) );
+		else
+			fprintf(stdout, "Processo filho terminou de forma anormal.\n");
+
+		/* Imprime dados na tela */
+		fprintf(stdout, "Processo pai. meu PID: %d.\n", (int)getpid() );
+		fprintf(stdout, "Processo pai. PID do processo filho anterior: %d.\n", (int)child_pid );
+		fprintf(stdout, "Iniciando processo de análise do áudio");
+		system("cd /home/pi/Desktop/Douglas");
+		system("sudo python microfone_db.py");
+		fprintf(stdout, "Audio analisado com sucesso!!!!");
+	}
+	return 0;
+}
+
