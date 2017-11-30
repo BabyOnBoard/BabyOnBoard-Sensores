93f3fafea114f158d67f8b4e5afe2ea9ecccc7c4
Jonathan Rufino <jonathan.rufinotkd@gmail.com>
2017-11-30 21:27:10 -0200

    backup

--- microfone.c
+++ microfone.c
@@ -5,51 +5,51 @@
 #include<sys/wait.h>		//lib para usar wait()
 
 
-
 int main(){
-  pid_t child_pid;
-  int status;
+    pid_t child_pid;
+    int status;
 
-	/* Cria processo filho */
-	child_pid = fork();
+    /* Cria processo filho */
+    child_pid = fork();
 
-	if( child_pid == 0 ){
-		/* Processo filho. */
-
-		fprintf(stdout, " Iniciando Processo Filho. Possui PID: %d.\n", (int)getpid() );
-		fprintf(stdout, "PID do Processo Pai: %d.\n", (int)getppid() );
-		fprintf(stdout, "Iniciando processo para gravação de audio pelo microfone");
-		system("cd /home/pi/Git/BabyOnBoard-Sensores/");
-		system("rec -r 48000 -c1 teste.wav trim 0 5");
+    if (child_pid == 0) {
+        /* Processo filho. */
+        fprintf(stdout, " Iniciando Processo Filho. Possui PID: %d.\n", (int)getpid() );
+	fprintf(stdout, "PID do Processo Pai: %d.\n", (int)getppid() );
+	fprintf(stdout, "Iniciando processo para gravação de audio pelo microfone");
+	system("cd /home/pi/Git/BabyOnBoard-Sensores/");
+	system("rec -r 48000 -c1 teste.wav trim 0 5");
 		//sleep(4);
-		_exit(EXIT_SUCCESS);
+	_exit(EXIT_SUCCESS);
+    }
+    else if (child_pid < 0) {
+        /* Se houver erro ao criar processo filho */
+	perror("fork");
+	exit(EXIT_FAILURE);
+    }
+    else {
+	/* Código do processo pai continua aqui */
+	/* Aguarda processo filho terminar e recebe status */
+	wait(& status);
 
-	}
-	else if( child_pid < 0 ){
-		/* Se houver erro ao criar processo filho */
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else{
-		/* Código do processo pai continua aqui */
-		/* Aguarda processo filho terminar e recebe status */
-		wait(& status);
+	/* Verifica se o processo filho terminou normalmente */
+	/* Caso tenha terminado, imprime o status de saída do filho */
+	if (WIFEXITED(status)) {
+	    fprintf(stdout, "Status de saida do filho: %d.\n", WEXITSTATUS(status) );
+        }
+	else {
+	    fprintf(stdout, "Processo filho terminou de forma anormal.\n");
+        }
 
-		/* Verifica se o processo filho terminou normalmente */
-		/* Caso tenha terminado, imprime o status de saída do filho */
-		if( WIFEXITED(status) )
-			fprintf(stdout, "Status de saida do filho: %d.\n", WEXITSTATUS(status) );
-		else
-			fprintf(stdout, "Processo filho terminou de forma anormal.\n");
+	/* Imprime dados na tela */
+	fprintf(stdout, "Processo pai. meu PID: %d.\n", (int)getpid() );
+	fprintf(stdout, "Processo pai. PID do processo filho anterior: %d.\n", (int)child_pid );
+	fprintf(stdout, "Iniciando processo de análise do áudio");
+	system("cd /home/pi/Git/BabyOnBoard-Sensores/");
+	system("sudo python3 ruido.py");
+	fprintf(stdout, "Audio analisado com sucesso!!!!");
+    }
 
-		/* Imprime dados na tela */
-		fprintf(stdout, "Processo pai. meu PID: %d.\n", (int)getpid() );
-		fprintf(stdout, "Processo pai. PID do processo filho anterior: %d.\n", (int)child_pid );
-		fprintf(stdout, "Iniciando processo de análise do áudio");
-		system("cd /home/pi/Git/BabyOnBoard-Sensores/");
-		system("sudo python3 ruido.py");
-		fprintf(stdout, "Audio analisado com sucesso!!!!");
-	}
-	return 0;
+    return 0;
 }
 
